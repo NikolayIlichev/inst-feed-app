c31c3b43c31a03c19b6b14701127aebf5552df32
Nickolay Ilichev <spcazaam@yandex.ru>
2020-03-16 18:14:16 +0000

    add styles for images output

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
+ini_set('error_reporting', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 // https://shielded-caverns-80867.herokuapp.com/
 // https://git.heroku.com/shielded-caverns-80867.git
+
+$token = 'IGQVJYeXB4WkxDNF9NTEFVaVpheGV5TWlJMUhCTG5NSS1qSnZAJM0dMQlVaM1pGWmp5NEVsMlp1TkQ5NmQ5M2UtNmRSelJ0Vy1mbHZAWVXRUQzhzMVd4MWZAKd05zdXpWX1J3b3V0YmJXZAWdmSlBteTVtaQZDZD';
+
+$instagram_cnct = curl_init(); // инициализация cURL подключения
+curl_setopt( $instagram_cnct, CURLOPT_URL, "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token={long-lived-access-token}" . $token ); // подключаемся
+curl_setopt( $instagram_cnct, CURLOPT_RETURNTRANSFER, 1 ); // просим вернуть результат
+curl_setopt( $instagram_cnct, CURLOPT_TIMEOUT, 15 );
+$response = json_decode( curl_exec( $instagram_cnct ) ); // получаем и декодируем данные из JSON
+curl_close( $instagram_cnct ); // закрываем соединение
+
+echo '<pre>';
+print_r($response);
+echo '</pre>';
+/*
 echo "It's a new inst-feed-app!";
 
 // new api
@@ -10,7 +27,7 @@ if (!empty($token))
 {
 	// get media data from token owner account
 	$instagram_cnct = curl_init(); // инициализация cURL подключения
-	curl_setopt( $instagram_cnct, CURLOPT_URL, "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp&access_token=" . $token ); // подключаемся
+	curl_setopt( $instagram_cnct, CURLOPT_URL, "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp,thumbnail_url&access_token=" . $token ); // подключаемся
 	curl_setopt( $instagram_cnct, CURLOPT_RETURNTRANSFER, 1 ); // просим вернуть результат
 	curl_setopt( $instagram_cnct, CURLOPT_TIMEOUT, 15 );
 	$media = json_decode( curl_exec( $instagram_cnct ) ); // получаем и декодируем данные из JSON
@@ -30,11 +47,24 @@ if (!empty($token))
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+
+	<style>
+		.images__wrapper {
+			display: flex;
+			flex-wrap: wrap;
+		}
+
+		.image__block {
+			width: 200px;
+    		height: 200px;
+    		background-size: cover;
+		}
+	</style>
 </head>
 <body>
-	<div>
+	<div class="images__wrapper">
 		<?php foreach($media->data as $key => $mediaObj): ?>
-		<img src="<?php echo $mediaObj->media_url; ?>" >
+		<div class="image__block" style="background-image: url(<?php echo $mediaObj->thumbnail_url ?: $mediaObj->media_url; ?>)"></div>
 		<?php endforeach; ?>
 	</div>
 </body>
