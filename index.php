54c87d3e4dc7902845e4e0b8c5d03ce7cef4e471
Nickolay Ilichev <spcazaam@yandex.ru>
2020-03-16 17:28:17 +0000

    add new token instagram api

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 echo "It's a new inst-feed-app!";
 
 // new api
-$token = 'IGQVJWQ3FRTmd6UVI2d1owMDZAHWWJyZA0FTYnpjRVh1dFpQNURxeGlDQTVSSE40MkF1djBudk02TjVXdW9kUWg5c0xOVFBvYXBjeWk0dElUOGpPU0xfT09ZAWTlvTnZAudlpsbGJRS1ZAGeFFCYjRCTGZAXbwZDZD';
+$token = 'IGQVJYeXB4WkxDNF9NTEFVaVpheGV5TWlJMUhCTG5NSS1qSnZAJM0dMQlVaM1pGWmp5NEVsMlp1TkQ5NmQ5M2UtNmRSelJ0Vy1mbHZAWVXRUQzhzMVd4MWZAKd05zdXpWX1J3b3V0YmJXZAWdmSlBteTVtaQZDZD';
 
 if (!empty($token)) 
 {
@@ -17,12 +17,7 @@ if (!empty($token))
 	curl_close( $instagram_cnct ); // закрываем соединение
 
 	// Refresh a long-lived Instagram User Access Token that is at least 24 hours old but has not expired. Refreshed tokens are valid for 60 days from the date at which they are refreshed.
-	// $instagram_cnct = curl_init(); // инициализация cURL подключения
-	// curl_setopt( $instagram_cnct, CURLOPT_URL, "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp&access_token=" . $token ); // подключаемся
-	// curl_setopt( $instagram_cnct, CURLOPT_RETURNTRANSFER, 1 ); // просим вернуть результат
-	// curl_setopt( $instagram_cnct, CURLOPT_TIMEOUT, 15 );
-	// $media = json_decode( curl_exec( $instagram_cnct ) ); // получаем и декодируем данные из JSON
-	// curl_close( $instagram_cnct ); // закрываем соединение
+	
 	echo "<pre>";
 	print_r($media);
 	echo "</pre>";
